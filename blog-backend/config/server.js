3c46fb5d4d495f1ffc42d3e1f0b3be9f353173d2
Melanie Almeida <almeida.melanie@gmail.com>
2021-08-25 21:59:22 +0200

    installing strapi

---
+++ blog-backend/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'cbd793d9d413a01743816a87ed8e6acb'),
+    },
+  },
+});
---
+++ blog-backend/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '055d3cc5-84e9-42ab-8afe-bc968ea8789d'
+};
